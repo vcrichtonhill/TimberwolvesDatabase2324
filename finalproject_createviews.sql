c1107966ad3247e2af27835652e86d084dd4537f
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2024-11-07 19:02:50 -0600

    updated views and added some queries

--- finalproject_createviews.sql
+++ finalproject_createviews.sql
@@ -7,4 +7,47 @@ and games.game_id = playerstats.game_id
 and name = 'Anthony Edwards'
 ;
 
--- Jaden stats
+-- Jaden McDaniels stats
+create or replace view jaden_stats as
+select name, points, assists, rebounds, game_date
+from player, playerstats, games
+where player.player_id = playerstats.player_id
+and games.game_id = playerstats.game_id
+and name = 'Jaden McDaniels'
+;
+
+-- Mike Conley stats
+create or replace view mike_stats as
+select name, points, assists, rebounds, game_date
+from player, playerstats, games
+where player.player_id = playerstats.player_id
+and games.game_id = playerstats.game_id
+and name = 'Mike Conley'
+;
+
+-- Rudy Gobert stats
+create or replace view rudy_stats as
+select name, points, assists, rebounds, game_date
+from player, playerstats, games
+where player.player_id = playerstats.player_id
+and games.game_id = playerstats.game_id
+and name = 'Rudy Gobert'
+;
+
+-- Karl-Anthony Towns stats
+create or replace view kat_stats as
+select name, points, assists, rebounds, game_date
+from player, playerstats, games
+where player.player_id = playerstats.player_id
+and games.game_id = playerstats.game_id
+and name = 'Karl-Anthony Towns'
+;
+
+-- Naz Reid stats
+create or replace view naz_stats as
+select name, points, assists, rebounds, game_date
+from player, playerstats, games
+where player.player_id = playerstats.player_id
+and games.game_id = playerstats.game_id
+and name = 'Naz Reid'
+;
--- finalproject_queries.sql
+++ finalproject_queries.sql
@@ -1,23 +1,10 @@
--- Views
-
--- Anthony Edwards stats
-create or replace view ant_stats as
-select name, points, assists, rebounds, game_date
-from player, playerstats, games
-where player.player_id = playerstats.player_id
-and games.game_id = playerstats.game_id
-and name = 'Anthony Edwards'
-;
-
--- Jaden stats
-
--- Queries
-select round(avg(points), 2) as avgpoints
+select round(avg(points), 2) as avg_points, round(avg(assists), 2) as avg_assists, round(avg(rebounds), 2) as avg_rebounds
 from ant_stats;
 
-select game_date, points
+select game_date, points, assists, rebounds
 from ant_stats
-where points >= 25;
+where points >= 25
+;
 
 
 select * from ant_stats;
